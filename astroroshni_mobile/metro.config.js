5d82d55ba771d7d47f2277efaaeb46f9ffb560c0
Tarun Yadav <tarun.yadav@gmail.com>
2025-11-29 13:00:42 +0530

    Update metro config

--- astroroshni_mobile/metro.config.js
+++ astroroshni_mobile/metro.config.js
@@ -1,5 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-const config = {};
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = config;
